d95a73f41febbec74a26bdbb42dd15792f072abe
Dara <darazadoroznaa2004@mail.ru>
2024-11-28 12:23:14 +0300

    123

--- front_edu_vault/src/components/SecretaryRegistration.jsx
+++ front_edu_vault/src/components/SecretaryRegistration.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import styles from '../styles/SecretaryRegistration.module.css';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
 
 const SecretaryRegistration = ({addSecretary, hideAddSecretary, create}) => {
 
